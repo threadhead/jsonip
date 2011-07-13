d9da1c1e65c5492dda7b6614d103ccbf15b75952
Jon McCartie <jon@mccartie.com>
2011-07-13 00:37:30 -0500

    Hosts-based restrictions

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "http://rubygems.org"
 gem "sinatra"
 
 group :test do
+  gem "shotgun"
   gem 'rspec', '=2.4.0'
   gem 'rack-test'
     gem 'awesome_print'
--- main.rb
+++ main.rb
@@ -4,6 +4,9 @@ require 'sinatra'
 require 'json'
 
 get '/' do
+  allowed_hosts = ENV["ALLOWED_HOSTS"].split(",") rescue []
+  return 403 unless allowed_hosts.include?(request.env["HTTP_REFERER"]) || Sinatra::Application.environment.to_s == 'development'
+
   content_type :json
   @info = {
     ip: request.ip
