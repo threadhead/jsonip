fc5bbac462c9a30aee1eb00a2082f0637bddcc74
Jon McCartie <jon@mccartie.com>
2011-07-13 13:17:51 -0500

    Simplified main. Added pending tests.

--- main.rb
+++ main.rb
@@ -4,20 +4,12 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-  allowed_hosts = ENV["ALLOWED_HOSTS"].split(",") rescue []
-  matches = request.env["HTTP_REFERER"].match(/http(?:s)?:\/\/(.*?)\/(.*?)/) rescue []
-  return 403 unless allowed_hosts.include?(matches[1]) || Sinatra::Application.environment.to_s == 'development'
+  @allowed_hosts = ENV["ALLOWED_HOSTS"].split(",") rescue []
+  @referer = request.env["HTTP_REFERER"].match(/http(?:s)?:\/\/(.*?)\/(.*?)/) rescue []
+  return 403 unless @allowed_hosts.include?(@referer[1]) || Sinatra::Application.environment.to_s == 'development'
 
   content_type :json
-  @info = {
-    ip: request.ip
-  }
+  @data = JSON.dump({ip: request.ip})
 
-  data = JSON.dump(@info)
-  if params[:callback]
-    "#{params[:callback]}(#{data});"
-  else
-    data
-  end
-
+  params[:callback].nil? ? @data : "#{params[:callback]}(#{@data});"
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,4 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "The Main App" do
 
+  it "is successful"
+  it "returns a 403 if the referer is not in the allowed list"
+  it "returns JSON of the requestor's IP'"
+  it "returns JSON inside a function if the callback param is used"
+
 end
